cda1bd5bb85c56eb5bc613827bee9a4a4a600206
root <mansurov.artem07@gmail.com>
2025-11-01 13:34:45 +0300

    13:34

--- main1.cpp
+++ main1.cpp
@@ -10,7 +10,7 @@ void desrtoy(int ** mtx, size_t created)
 
 int ** create(size_t rows, size_t cols)
 {
-int ** mtx - new int *[rows];
+int ** mtx = new int *[rows];
 suze_t created = 0;
 try
 {
@@ -19,7 +19,7 @@ try
   mtx[i] = new int[cols];
  }
 }
-catch (const std::bad_alloc & e)
+catch (const std:: bad_alloc &e)
 {
  destroy(mtx, created);
  throw;
@@ -27,15 +27,28 @@ catch (const std::bad_alloc & e)
 return mtx;
 }
 
+void construct(int ** mtx, size_t rows, size_t cols, int init)
+{
+ for (size_t i = 0; i < rows; ++i)
+ {
+  for (size_t j = 0; j < cols; ++j)
+   {
+     mtx[i][j] = init;
+   }
+ }
+}
+
 int main()
 {
+size_t r = 5, c = 5;
 int ** matrix = nullptr;
 try{
  matrix = create(5,5);
 }
-catch(const std::bad_allloc &e)
+catch(const std::bad_alloc &e)
 {
  std::cerr << e.what() << "\n";
  return 1;
 }
+
 }
