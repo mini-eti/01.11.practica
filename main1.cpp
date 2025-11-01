dd6e6e83ea262e01d8b16d162211c8ce292b2967
root <mansurov.artem07@gmail.com>
2025-11-01 13:05:52 +0300

    destroy and create

--- main1.cpp
+++ main1.cpp
@@ -1,2 +1,41 @@
-int main(){
+#include <iostream>
+void desrtoy(int ** mtx, size_t created)
+{
+ for(size_t i = 0; i < created; i++)
+ {
+   delete[] mtx[i]
+ }
+ delete[] mtx;
+}
+
+int ** create(size_t rows, size_t cols)
+{
+int ** mtx - new int *[rows];
+suze_t created = 0;
+try
+{
+ for(; created < rows; ++created)
+ {
+  mtx[i] = new int[cols];
+ }
+}
+catch (const std::bad_alloc & e)
+{
+ destroy(mtx, created);
+ throw;
+}
+return mtx;
+}
+
+int main()
+{
+int ** matrix = nullptr;
+try{
+ matrix = create(5,5);
+}
+catch(const std::bad_allloc &e)
+{
+ std::cerr << e.what() << "\n";
+ return 1;
+}
 }
